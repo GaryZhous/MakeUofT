3564923e5a492a2a84e695a57de284630e76e058
Joe Dai <dm.joedai@gmail.com>
2023-02-18 18:44:39 -0500

    Server - should be done

--- Server/main.py
+++ Server/main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, abort
+from markupsafe import escape
+from datetime import datetime, timedelta
+
 import crud
 
 
@@ -12,8 +15,37 @@ def hello_world():
 
 @app.route('/api/create/<string:room_id>')
 def create_room(room_id):
-    pass
+    room = crud.Room(room_id)
+    crud.create(room)
+    return f'Room <code>{escape(room_id)}</code> created!'
+
+
+@app.route('/api/moved/<string:room_id>')
+def moved(room_id):
+    room = crud.read(room_id)
+
+    if room is None:
+        abort(404)
+
+    room.moved()
+    crud.update(room)
+
+    return f'Recorded <code>{escape(room_id)}</code> movement!'
+
+
+@app.route('/api/need_move/<string:room_id>')
+def need_move(room_id):
+    room = crud.read(room_id)
+
+    if room is None:
+        abort(404)
+
+    if datetime.now() - room.last_moved > timedelta(hours=2):
+        return 'Need move!'
+    else:
+        return 'OK!'
 
 
 if __name__ == '__main__':
+    crud.init_db()
     app.run('0.0.0.0')
