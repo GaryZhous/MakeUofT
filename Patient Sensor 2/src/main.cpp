9fbdc2c7ab78689fe0f2d8e7d3756c0f293f256e
Joe Dai <dm.joedai@gmail.com>
2023-02-19 08:52:25 -0500

    Patient Sensor 2 - tweak thresholds

--- Patient Sensor 2/src/main.cpp
+++ Patient Sensor 2/src/main.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#define LED_PIN 16
 
 Adafruit_MPU6050 mpu;
+HTTPClient http;
+WiFiClient wifi;
+int triggered = 0;
+
+void sendRequest() {
+  http.begin(wifi, "http://10.0.0.1:8080/api/moved/100");
+  http.GET();
+  http.end();
+}
 
 void setup(void) {
   Serial.begin(115200);
@@ -21,66 +34,21 @@ void setup(void) {
   Serial.println("MPU6050 Found!");
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
 
   Serial.println("");
-  delay(100);
+
+  pinMode(LED_PIN, OUTPUT);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("RAPID", "helloworld");
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+  }
 }
 
 void loop() {
@@ -88,27 +56,42 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+  double x = 0, y = 0, z = 0;
+  for (int n = 0; n < 50; n++) {    // 250 ms averaging
+    x += g.gyro.x;
+    y += g.gyro.y;
+    z += g.gyro.z;
+    delay(5);
+  }
+  x /= 50;
+  y /= 50;
+  z /= 50;
+  x = abs(x);
+  y = abs(y);
+  z = abs(z);
+  Serial.println(x);
+  Serial.println(y);
+  Serial.println(z);
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
+  if (x > 0.8 || y > 0.8 || z > 0.8) {
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("Movement!");
+    triggered += 1;
+  }
+  else {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("No movement.");
+    triggered = 0;
+  }
 
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
-
-  Serial.println("");
-  delay(500);
+  if (triggered >= 8) {    // 2 s min
+    sendRequest();
+    for (int i = 0; i < 10; i++) {
+      digitalWrite(LED_PIN, LOW);
+      delay(25);
+      digitalWrite(LED_PIN, HIGH);
+      delay(25);
+    }
+    triggered = 0;
+  }
 }
