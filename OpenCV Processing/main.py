cc64b198b278ea622f301bb97917bcad7f417c5b
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2023-02-19 08:48:01 -0500

    Dropping ML and added threshold for OpenCV Processing

--- OpenCV Processing/main.py
+++ OpenCV Processing/main.py
@@ -1,61 +1,73 @@
 import cv2
-# from flask import Flask
-# from flask import Response
-# from flask import render_template
-# import threading
-#
-# outputFrame = None
-# lock = threading.Lock()
-#
-# app = Flask(__name__)
+import tensorflow as tf
+import numpy as np
+from time import time
+
+move = False
+moveCounter = 0
+
+time_of_motion = 5
+fps = 30
+
+width = 128
+height = 128
+
+time_no_motion = time()
+time_motion = time()
+
+output_file = "./data/move1.dat"
 
 def main():
     cap = cv2.VideoCapture("http://10.0.0.167:81/stream")
     # cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, fps)
     render_loop(cap)
     cap.release()
     cv2.destroyAllWindows()
 
 def render_loop(cap):
-    _, prev_frame = cap.read()
+    frame1 = get_frame(cap)
     while not pressed_esc():
-        prev_frame = display_delta_and_update_frame(cap, prev_frame)
+        frame1 = display_process_update_frame(cap, frame1)
 
-def display_delta_and_update_frame(cap, prev_frame):
-    global outputFrame, lock
-    _, new_frame = cap.read()
-    diff_frame = cv2.absdiff(src1=new_frame, src2=prev_frame)
-    cv2.imshow('webcam', diff_frame)
-    # with lock:
-    #     outputFrame = diff_frame.copy()
-    return new_frame
+def display_process_update_frame(cap, frame1):
+    # global outputFrame, lock
+    global move, time_motion, time_no_motion, time_of_motion
+    frame2 = get_frame(cap)
+    kernel = np.ones((2, 2))
+    diff = cv2.absdiff(src1=frame1, src2=frame2)
+    diff = cv2.dilate(diff, kernel, 1)
+    thresh_frame = cv2.threshold(src=diff, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+    value = thresh_frame.sum()
+    cv2.imshow('webcam', thresh_frame)
+    newMove = value > 10000
+
+    if newMove and move:
+        time_motion = time()
+        print("move")
+    else:
+        time_motion = time()
+        time_no_motion = time()
+        print("no move")
+    
+    if time_motion - time_no_motion > time_of_motion:
+        time_no_motion = time()
+        print("RESET")
+        #--------------------------
+        # send_reset()
+        #--------------------------
+    
+    move = newMove
+    return frame2
 
 def pressed_esc():
     return cv2.waitKey(30) == 27
 
-# @app.route("/")
-# def index():
-# 	return render_template("index.html")
-#
-# def generate():
-#     global outputFrame, lock
-#     while True:
-#         with lock:
-#             if outputFrame is None:
-#                 continue
-#             (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-#             # if not flag:
-#             #     continue
-#             yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
-#
-# @app.route("/video_feed")
-# def video_feed():
-# 	return Response(generate(),
-# 		mimetype = "multipart/x-mixed-replace; boundary=frame")
+def get_frame(cap):
+    _, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.resize(frame, (width, height))
+    return frame
 
 if __name__ == '__main__':
-    # t = threading.Thread(target=main)
-    # t.daemon = True
-    # t.start()
-    # app.run(host='localhost', port=8000, debug=True, threaded=True, use_reloader=False)
     main()
